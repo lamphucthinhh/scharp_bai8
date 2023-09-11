0adb4b09d701228bb2730e96e3e75562d086721b
ADMIN <ADMIN@LAB1-MAY31>
2023-09-11 16:14:32 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace scharp_bai8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtamlich = new System.Windows.Forms.TextBox();
+            this.txtduonglich = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(238, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tính Can chi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Năm dương lịch";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(80, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 31);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Năm âm lịch";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtamlich
+            // 
+            this.txtamlich.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Millimeter, ((byte)(0)));
+            this.txtamlich.Location = new System.Drawing.Point(334, 190);
+            this.txtamlich.Name = "txtamlich";
+            this.txtamlich.Size = new System.Drawing.Size(218, 43);
+            this.txtamlich.TabIndex = 2;
+            // 
+            // txtduonglich
+            // 
+            this.txtduonglich.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Millimeter, ((byte)(0)));
+            this.txtduonglich.Location = new System.Drawing.Point(334, 110);
+            this.txtduonglich.Name = "txtduonglich";
+            this.txtduonglich.Size = new System.Drawing.Size(218, 43);
+            this.txtduonglich.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(733, 185);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ghi gì cũng được";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtduonglich);
+            this.Controls.Add(this.txtamlich);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtamlich;
+        private System.Windows.Forms.TextBox txtduonglich;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scharp_bai8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        String Can, Chi;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (Convert.ToInt32(txtduonglich.Text.Substring(3,1)))
+            {
+                case 0:
+                    Can = "Canh";
+                    break;
+                case 1:
+                    Can = "tam";
+                    break;
+                case 2:
+                    Can = "nham";
+                    break;
+                case 3:
+                    Can = " Quy";
+                    break;
+                case 4:
+                    Can = " giáp";
+                    break;
+                case 5:
+                    Can = "Ất";
+                    break;
+                case 6:
+                    Can = "Bính";
+                    break;
+                case 7:
+                    Can = "Đinh";
+                    break;
+                case 8:
+                    Can = "Mậu";
+                    break;
+                case 9:
+                    Can = "kỵ";
+                    break;
+
+            }
+            switch (Convert.ToInt32(txtduonglich.Text) % 12)
+            {
+
+                case 0:
+                    Chi = "thân";
+                    break;
+                case 1:
+                    Chi = "Dậu";
+                    break;
+                case 2:
+                    Chi = "Tuất";
+                    break;
+                case 3:
+                    Chi = "Hợi";
+                    break;
+                case 4:
+                    Chi = "tý";
+                    break;
+                case 5:
+                    Chi = "sửu";
+                    break;
+                case 6:
+                    Chi = "Dần";
+                    break;
+                case 7:
+                    Chi = "Mão";
+                    break;
+                case 8:
+                    Chi = "Thìn";
+                    break;
+                case 9:
+                    Chi = "tị";
+                    break;
+               case 10:
+                    Chi = "Ngọ";
+                    break;
+                case 11:
+                    Chi = "Mùi";
+                    break;
+            }
+            txtamlich.Text = Can + " " + Chi;
+        }
+    }
+}
